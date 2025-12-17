0189afef69f82220dd878d956aff5e1bf259ecde
Sneha Pal <118155232+SnehaPal16@users.noreply.github.com>
2025-12-18 01:01:46 +0530

    !00% completed

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
@@ -20,6 +21,8 @@ mongoose
   const __dirname = path.resolve();
 
 const app = express();
+
+app.use(cors())
 
 app.use(express.json());
 
